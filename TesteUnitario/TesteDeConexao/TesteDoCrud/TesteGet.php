98d3b900f8b53c0c25d29d42d8661c09e409ef5b
Julio Cezar <134234797+juliocezar2310@users.noreply.github.com>
2023-12-04 17:21:40 -0300

    Add files via upload

---
+++ TesteUnitario/TesteDeConexao/TesteDoCrud/TesteGet.php
@@ -0,0 +1,69 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseGetTest extends TestCase
+{
+    public function testSuccessfulGet()
+    {
+        $host = "localhost";
+        $db = "base_php";
+        $user = "postgre";
+        $pass = "";
+
+        // Simulando um ID válido para busca
+        $_GET['id'] = 1;
+
+        try {
+            $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $id = $_GET['id'];
+
+            $sql = 'SELECT * FROM tabela WHERE id = :id';
+
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+            $stmt->execute();
+
+            $this->assertNotNull($stmt->fetch(PDO::FETCH_ASSOC));
+        } catch (PDOException $e) {
+            $this->fail("Erro durante a busca: " . $e->getMessage());
+        }
+    }
+
+    public function testFailedGet()
+    {
+        $host = "localhost";
+        $db = "base_php";
+        $user = "postgre";
+        $pass = "";
+
+        // Simulando um ID inválido para busca
+        $_GET['id'] = 9999;
+
+        try {
+            $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $id = $_GET['id'];
+
+            $sql = 'SELECT * FROM tabela WHERE id = :id';
+
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+            $stmt->execute();
+
+            $this->assertNull($stmt->fetch(PDO::FETCH_ASSOC));
+        } catch (PDOException $e) {
+            $this->fail("Erro durante a busca: " . $e->getMessage());
+        }
+    }
+}
+?>
